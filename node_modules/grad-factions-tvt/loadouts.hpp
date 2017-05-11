6e72c34c33d68e245e2baa1bf442589bf09a10b5
McDiod <hobbeldobbel@gmail.com>
2017-05-11 17:58:11 +0200

    add grad-factions-tvt

---
+++ node_modules/grad-factions-tvt/loadouts.hpp
@@ -0,0 +1,7 @@
+#include "list_macros.hpp"
+
+#include "factions\ger_heer_w.hpp"
+#include "factions\ger_heer_d.hpp"
+#include "factions\rus_msv_w.hpp"
+#include "factions\rus_msv_d.hpp"
+#include "factions\us_army.hpp"
