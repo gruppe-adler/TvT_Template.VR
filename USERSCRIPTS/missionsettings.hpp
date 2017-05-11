5ef652e373f9bf15a9d91e3cc08902b910378949
McDiod <hobbeldobbel@gmail.com>
2017-05-11 18:52:48 +0200

    more user config files

--- USERSCRIPTS/missionsettings.hpp
+++ USERSCRIPTS/missionsettings.hpp
@@ -2,5 +2,17 @@
 *
 */
 
+//ALLGEMEIN ====================================================================
 canTakeEnemyRadios = 0;                                                         // Können Spieler feindliche Funkgeräte aufsammeln? (0/1)
 canUseMarksmanScopes = 0;                                                       // Können Spieler vergrößernde Visiere auf allen Waffen benutzen? (0/1) - siehe scopes.hpp
+
+
+//LOADOUTS =====================================================================
+loadoutFactionBlu = "";                                                         //Partei, dessen Standardloadout die Bluforspieler bekommen
+loadoutFactionOpf = "";                                                         //Partei, dessen Standardloadout die Opforspieler bekommen
+loadoutFactionInd = "";                                                         //Partei, dessen Standardloadout die Independentspieler bekommen
+
+//#define NIGHTVISION                                                           //Wenn Spieler Nightvision erhalten sollen, Kommentarzeichen (//) entfernen
+//#define GUNLIGHTS                                                             //Wenn Spieler Waffenlampen erhalten sollen, Kommentarzeichen (//) entfernen
+//#define LASERS                                                                //Wenn Spieler Waffenlaser erhalten sollen, Kommentarzeichen (//) entfernen
+//#define SUPPRESSORS                                                           //Wenn Spieler Schalldämpfer erhalten sollen, Kommentarzeichen (//) entfernen
