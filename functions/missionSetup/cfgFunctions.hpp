83c325cfe1ecbcdeb84d96c7b55b39562af7238f
McDiod <hobbeldobbel@gmail.com>
2017-09-08 20:40:01 +0200

    update grad-civs

--- functions/missionSetup/cfgFunctions.hpp
+++ functions/missionSetup/cfgFunctions.hpp
@@ -2,6 +2,7 @@ class grad_missionSetup {
     class missionSetup {
         class createDiaryRecords {};
         class initCivs {};
+        class initCivsEquip {preInit = 1;};
         class initMission {postInit = 1;};
         class initVars {preInit = 1;};
         class intro {};
