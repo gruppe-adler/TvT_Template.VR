04eaeddff85c6b682feb80d6b6a387d2300d259b
Moritz Schmidt <fusselwurm@gmail.com>
2018-04-03 20:57:16 +0200

    improve win conditions

--- USER/winConditions.hpp
+++ USER/winConditions.hpp
@@ -3,9 +3,9 @@ class bluforeliminated {
     presetParams[] = {"WEST"};
 
     condition = "";
-    checkInterval = 0;
+    checkInterval = 2;
 
-    winners[] = {"EAST","INDEPENDENT"};
+    winners[] = {"EAST"};
     winText = "BLUFOR eliminated after %1 minutes.<br/>OPFOR wins!";
     winTextParams[] = {"round (CBA_missionTime/60)"};
 };
@@ -14,9 +14,9 @@ class opforeliminated {
     presetParams[] = {"EAST"};
 
     condition = "";
-    checkInterval = 0;
+    checkInterval = 2;
 
-    winners[] = {"WEST","INDEPENDENT"};
+    winners[] = {"WEST"};
     winText = "OPFOR eliminated after %1 minutes.<br/>BLUFOR wins!";
     winTextParams[] = {"round (CBA_missionTime/60)"};
 };
