0299565b74c3fec30eaa62174ebad157837df42f
Moritz Schmidt <fusselwurm@gmail.com>
2018-04-21 13:09:01 +0200

    add points win conddition

--- USER/winConditions.hpp
+++ USER/winConditions.hpp
@@ -20,3 +20,27 @@ class opforeliminated {
     winText = "OPFOR eliminated after %1 minutes.<br/>BLUFOR wins!";
     winTextParams[] = {"round (CBA_missionTime/60)"};
 };
+
+class bluforWinPoints {
+    preset = "POINTS";
+    presetParams[] = {"WEST", 1200, 3600};
+
+    condition = "";
+    checkInterval = 1;
+
+    winners[] = {"WEST"};
+    winText = "BLUFOR wins by points!<br/>OPFOR only had %1 points.";
+    winTextParams[] = {"[EAST] call grad_common_fnc_getPoints"};
+};
+
+class opforWinPoints {
+    preset = "POINTS";
+    presetParams[] = {"EAST", 15000, 9999};
+
+    condition = "";
+    checkInterval = 1;
+
+    winners[] = {"EAST"};
+    winText = "EAST wins by points!<br/>BLUFOR only had %1 points.";
+    winTextParams[] = {"[WEST] call grad_common_fnc_getPoints"};
+};
