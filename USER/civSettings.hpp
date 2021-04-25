fc3f884c92363960e22db6e48c53bf05f047d472
Moritz Schmidt <fusselwurm@gmail.com>
2021-04-25 20:50:45 +0200

    more civ settings, remove old commentary

--- USER/civSettings.hpp
+++ USER/civSettings.hpp
@@ -9,13 +9,17 @@ class userEquipment {
 
     // Uniformen
     clothes[] = {
-        "LOP_U_TAK_Civ_Fatigue_01",
-        "LOP_U_TAK_Civ_Fatigue_02",
-        "LOP_U_TAK_Civ_Fatigue_06",
-        "LOP_U_TAK_Civ_Fatigue_08",
-        "LOP_U_TAK_Civ_Fatigue_10",
-        "LOP_U_TAK_Civ_Fatigue_11",
-        "LOP_U_TAK_Civ_Fatigue_14"
+        "UK3CB_ADC_C_U_01",
+        "UK3CB_ADC_C_U_01_B",
+        "UK3CB_ADC_C_U_01_C",
+        "UK3CB_ADC_C_U_01_D",
+        "UK3CB_ADC_C_U_01_E",
+        "UK3CB_ADC_C_U_01_F",
+        "UK3CB_ADC_C_U_01_G",
+        "UK3CB_ADC_C_U_01_H",
+        "UK3CB_ADC_C_U_01_I",
+        "UK3CB_ADC_C_U_01_J",
+        "UK3CB_ADC_C_U_01_K"
     };
 
     // Helme, Mützen
