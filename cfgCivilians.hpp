405d9b7172697993f6922decac5d36185e795723
Moritz Schmidt <fusselwurm@gmail.com>
2021-04-25 20:47:58 +0200

    civ middle eastern uniforms & headgear

--- cfgCivilians.hpp
+++ cfgCivilians.hpp
@@ -183,26 +183,23 @@ class cfgCivilians {
 
     class middleEastern {
         clothes[] = {
-            "LOP_U_TAK_Civ_Fatigue_06",
-            "LOP_U_TAK_Civ_Fatigue_08",
-            "LOP_U_TAK_Civ_Fatigue_07",
-            "LOP_U_TAK_Civ_Fatigue_05",
-            "LOP_U_TAK_Civ_Fatigue_01",
-            "LOP_U_TAK_Civ_Fatigue_10",
-            "LOP_U_TAK_Civ_Fatigue_02",
-            "LOP_U_TAK_Civ_Fatigue_09",
-            "LOP_U_TAK_Civ_Fatigue_11",
-            "LOP_U_TAK_Civ_Fatigue_12",
-            "LOP_U_TAK_Civ_Fatigue_04",
-            "LOP_U_TAK_Civ_Fatigue_14",
-            "LOP_U_TAK_Civ_Fatigue_13",
-            "LOP_U_TAK_Civ_Fatigue_16",
-            "LOP_U_TAK_Civ_Fatigue_15"
+            "UK3CB_ADC_C_U_01",
+            "UK3CB_ADC_C_U_01_B",
+            "UK3CB_ADC_C_U_01_C",
+            "UK3CB_ADC_C_U_01_D",
+            "UK3CB_ADC_C_U_01_E",
+            "UK3CB_ADC_C_U_01_F",
+            "UK3CB_ADC_C_U_01_G",
+            "UK3CB_ADC_C_U_01_H",
+            "UK3CB_ADC_C_U_01_I",
+            "UK3CB_ADC_C_U_01_J",
+            "UK3CB_ADC_C_U_01_K"
         };
 
         headgear[] = {
-            "LOP_H_Pakol",
-            "LOP_H_Turban"
+            "UK3CB_TKC_H_Turban_01_1",
+            "UK3CB_TKC_H_Turban_02_1",
+            "UK3CB_TKC_H_Turban_06_1"
         };
 
         faces[] = {
