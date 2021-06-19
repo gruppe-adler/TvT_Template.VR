ed3b70d4d4da199237cdc9294b368123108a46b4
Moritz Schmidt <fusselwurm@gmail.com>
2021-06-19 15:54:22 +0200

    add respawn settings

--- USER/respawnSettings.hpp
+++ USER/respawnSettings.hpp
@@ -1,22 +1,22 @@
 //WAVE RESPAWN =================================================================
-waveRespawnEnabled = 0;                                                         //Wave Respawn ein-/ausschalten (1/0)
+waveRespawnEnabled = 1;                                                         //Wave Respawn ein-/ausschalten (1/0)
 
 bluforWaveSize = -1;                                                            //Wellengröße Blufor (-1 == automatisch)
 opforWaveSize = -1;                                                             //Wellengröße Opfor (-1 == automatisch)
 indepWaveSize = -1;                                                             //Wellengröße Independent (-1 == automatisch)
 
-bluforWaveLifes = 9999;                                                         //Wie oft ein Blufor Spieler respawnen kann
-opforWaveLifes = 9999;                                                          //Wie oft ein Opfor Spieler respawnen kann
-indepWaveLifes = 9999;                                                          //Wie oft ein Independent Spieler respawnen kann
+bluforWaveLifes = 2;                                                         //Wie oft ein Blufor Spieler respawnen kann
+opforWaveLifes = 2;                                                          //Wie oft ein Opfor Spieler respawnen kann
+indepWaveLifes = 0;                                                          //Wie oft ein Independent Spieler respawnen kann
 
 bluforInterruptCondition = "false";                                             //Bedingung nach der der Blufor Waverespawn deaktiviert wird
 opforInterruptCondition = "false";                                              //Bedingung nach der der Opfor Waverespawn deaktiviert wird
 indepInterruptCondition = "false";                                              //Bedingung nach der der Independent Waverespawn deaktiviert wird
 
-waverespawntimePlayer = 30;                                                     //Spielerrespawnzeit, bevor er der Welle hinzugefügt wird
+waverespawntimePlayer = 60;                                                     //Spielerrespawnzeit, bevor er der Welle hinzugefügt wird
 
-waverespawntimeBlu = 30;                                                        //Wellenrespawnzeit Blufor in Sekunden
-waverespawntimeOpf = 30;                                                        //Wellenrespawnzeit Opfor in Sekunden
+waverespawntimeBlu = 600;                                                        //Wellenrespawnzeit Blufor in Sekunden
+waverespawntimeOpf = 600;                                                        //Wellenrespawnzeit Opfor in Sekunden
 waverespawntimeInd = 30;                                                        //Wellenrespawnzeit Independent in Sekunden
 
 
